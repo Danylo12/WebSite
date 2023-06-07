f702a529cddb5b67885fb419d19f2cfc5faa60ce
Danylo12 <97602602+Danylo12@users.noreply.github.com>
2023-06-07 19:17:54 +0300

    Update server.py

--- back/server.py
+++ back/server.py
@@ -1,1 +1,19 @@
+from flask import Flask, request, render_template, url_for
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html') #render main html file
+
+@app.route('/login')
+def login():
+    return render_template('login.html') #render login page
+
+@app.route('/register')
+def register():
+    return render_template('register.html') #render register page
+
+
+if __name__== "__main__":
+    app.run(debug=True, host='localhost', port=9874)
